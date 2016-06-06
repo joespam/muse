56fd610d2f5b86788bc752a820e55718582e8b23
Joe Spampinato <jcspampinato@gmail.com>
2016-06-06 16:06:11 -0400

    Added random styling, allowed username to be editable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.for(:account_update) << :name ## add the attributes you want to permit
   end
 end
